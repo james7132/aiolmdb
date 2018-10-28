29b2be4226cfc2a82960786964abf3e137b7898e
james7132 <contact@jamessliu.com>
2018-10-27 22:12:48 -0700

    Add tags for the specific python 3 versions supported

--- setup.py
+++ setup.py
@@ -15,6 +15,10 @@ setuptools.setup(
     packages=setuptools.find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.4",
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
